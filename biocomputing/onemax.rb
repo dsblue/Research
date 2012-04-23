4774a039815c112d5387bcb1b72246984e6c458e
Nathan Palmer <dsblue@gmail.com>
2012-04-23 10:33:10 -0500

    Clean up

--- biocomputing/onemax.rb
+++ biocomputing/onemax.rb
@@ -1,30 +1,5 @@
 N = ARGV[0].to_i
 
-def population(n) 
-  pop = Array.new
-  for i in (1..n)
-    pop[i-1] = rand(2)
-  end
-  return pop
-end
-
-def fitness(p)
-  ones = 0;
-  p.each { |i| 
-    if i == 1
-      ones = ones + 1
-    end
-  }
-  return ones / (1.0 * p.size)
-end
-
-def prune(s)
-  
-end
-
-##
-##
-##
 class CantidateSolution
   attr_accessor :size
   
@@ -66,8 +41,6 @@ class Population
     end
   end
 
-  
-
   def print
     @population.each { |s|
       p s,s.fitness
@@ -76,26 +49,8 @@ class Population
 
 end
 
-s1 = CantidateSolution.new(10)
-
-p s1.to_s
-p s1.fitness
-
-p1 = Population.new(5,2)
-
-p1.print
-
 #######################################
 
-solutions = Array.new(N)
-fitgroup = Array.new
+p1 = Population.new(100,N)
 
-for s in (0..solutions.size-1)
-  solutions[s] = population(1000)     
-end
-
-solutions.each { |s|  
-   fitgroup.push(fitness(s))
-}
-
-#p fitgroup.sort
+p1.print
