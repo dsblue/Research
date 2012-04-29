c611247ac549c2908ed6ed225f1461f57ec13b42
Nathan Palmer <dsblue@gmail.com>
2012-04-29 18:11:31 -0500

    Finished simple onemax

--- biocomputing/onemax.rb
+++ biocomputing/onemax.rb
@@ -1,7 +1,16 @@
-N = ARGV[0].to_i
+STRLEN = ARGV[0].to_i
+POPSIZE = ARGV[1].to_i
+
+## What percent to discard each itteration
+#DISCARD = 0.50
+DISCARD = ARGV[2].to_i * 0.01
+## Mutation rate
+#MUTATE = 0.10
+MUTATE = ARGV[3].to_i * 0.01
 
 class CantidateSolution
   attr_accessor :size
+  attr_accessor :solution
   
   def initialize(size)
     @size = size
@@ -26,6 +35,41 @@ class CantidateSolution
     return ones / (1.0 * @size)
   end
 
+  def breed(mate)
+  
+    point = rand(@solution.size)
+    child = CantidateSolution.new(@solution.size)
+    start_mom = rand(2)
+    for i in (0..@solution.size-1) 
+      if (i < point) 
+        if (start_mom == 0)
+          child.solution[i] = @solution[i]      
+        else
+          child.solution[i] = mate.solution[i]
+        end
+      else 
+        if (start_mom == 0)
+          child.solution[i] = mate.solution[i]
+        else
+          child.solution[i] = @solution[i]    
+        end
+      end
+    end
+
+    ## Add point mutation
+    point = rand(@solution.size)
+    if ( rand() < MUTATE )
+      if ( child.solution[point] == 1 )
+        child.solution[point] = 0
+      else 
+        child.solution[point] = 1
+      end
+    end
+        
+    return child
+    
+  end
+
 end
 
 class Population
@@ -43,14 +87,56 @@ class Population
 
   def print
     @population.each { |s|
-      p s,s.fitness
+      p s.fitness
     }
+  end
+
+  def itterate
+
+    ## Sort the population by fitness
+    @population = @population.sort { |a, b| a.fitness <=> b.fitness  }
+  
+    ## Print best
+    ##p @population[@population.size-1]
+
+    ## Replace the least fit by offspring of the most fit
+    mid = (@population.size * DISCARD).floor
+    best = (@population.size-1) - mid
+
+    for i in (mid..@population.size-1)
+      mom = @population[mid + rand(best)]
+      dad = @population[mid + rand(best)]    
+      child = mom.breed(dad)
+      @population[i] = child  
+    end
+  
+  end
+
+  def check
+    max = false
+    @population.each { |s|
+      if ( s.fitness == 1.0 )
+#        puts "Found solution: #{ s.to_s } "
+        max = true
+      end
+    }
+    return max
   end
 
 end
 
 #######################################
 
-p1 = Population.new(100,N)
+p1 = Population.new(POPSIZE,STRLEN)
 
-p1.print
+i = 0;
+while !p1.check
+#  puts "Itteration ##{i}"
+  i = i + 1
+  p1.itterate
+end
+
+puts "Itteration ##{i}"
+ 
+#p1.print
+
